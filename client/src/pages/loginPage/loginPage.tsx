12d1c4e77d1c285c411aaf1e6e667a7b3d5ba232
Luisgsm324 <luisf.moreira324@gmail.com>
2025-01-26 21:16:55 -0300

    [fix]: Correções do pull request

--- client/src/pages/loginPage/loginPage.tsx
+++ client/src/pages/loginPage/loginPage.tsx
@@ -6,35 +6,35 @@ import "./loginPageStyle.css";
 function LoginPage () {
     return(        
         <form className="login-bg">
-        <div className="loginPage-container">            
-            <Logo/>
-            <h2>Login</h2>
-                <InputForm 
-            placeholderText="email@email.com"
-            inputId="email"
-            labelTitle="Email"
-            typeInput="text" />
-            
-                <InputForm 
-            placeholderText="***********"
-            inputId="password"
-            labelTitle="Senha"
-            typeInput="password" />       
-            
-            <FormButton 
-            content="Entrar"/>
-            
-            <div className="bottom-paragraph-login-elements">
-                <div>
-                    <a>Esqueci a senha</a>
-                </div>
+            <div className="loginPage-container">            
+                <Logo/>
+                <h2>Login</h2>
+                    <InputForm 
+                placeholderText="email@email.com"
+                inputId="email"
+                labelTitle="Email"
+                typeInput="text" />
+                
+                    <InputForm 
+                placeholderText="***********"
+                inputId="password"
+                labelTitle="Senha"
+                typeInput="password" />       
+                
+                <FormButton 
+                content="Entrar"/>
+                
+                <div className="bottom-paragraph-login-elements">
+                    <div>
+                        <a>Esqueci a senha</a>
+                    </div>
 
-                <div className="register-text">                    
-                    <a>Não tem uma conta? Cadastre-se 🌲</a>
+                    <div className="register-text">                    
+                        <a>Não tem uma conta? Cadastre-se 🌲</a>
+                    </div>
+                
                 </div>
-            
             </div>
-        </div>
         </form> 
     );
 
