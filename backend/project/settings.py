aa323bb2d1beacb747c10c11079e7cbcafe3b286
Felipe Santos <felipessantos2004@gmail.com>
2025-04-02 20:19:06 -0300

    feat: email server configuration

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -149,7 +149,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # E-mail server
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'djangoapirestful@gmail.com'
+EMAIL_HOST_PASSWORD = 'jlzl lecn uzdp rihz'
 
 
 # Django REST Framework
