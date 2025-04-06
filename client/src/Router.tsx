54e7f40deb098d0fbfa9d23be2f6941ca779e551
Luisgsm324 <luisf.moreira324@gmail.com>
2025-04-05 22:35:15 -0300

    [feat]: Adição da estrutura da tela de home na branch

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -3,16 +3,18 @@ import LoginPage from "./pages/loginPage/loginPage";
 import RegistrationPage from "./pages/registerPage/registerPage";
 import UpdatePasswordPage from "./pages/updatePasswordPage/updatePasswordPage";
 import ForgotPasswordPage from "./pages/forgotPasswordPages/forgotPasswordPage";
+import HomeScreenPage from "./pages/homeScreenPage/homeScreenPage";
 
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<RegistrationPage/>} />
         <Route path="/updatepassword" element={<UpdatePasswordPage/>} />
         <Route path="/forgotpassword" element={<ForgotPasswordPage/>} />
+        <Route path="/" element={<HomeScreenPage/>} />
       </Routes>
     </BrowserRouter>
   );
